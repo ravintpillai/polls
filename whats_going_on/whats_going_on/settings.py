1c8e4039a827ec6bb6b9852b013eab2618765f41
Ravin Thambapillai <rthambapillai@palantir.com>
2017-09-18 01:23:03 +0300

    try setting up postgres

--- whats_going_on/whats_going_on/settings.py
+++ whats_going_on/whats_going_on/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'whats_going_on.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'whats_going_on_db',
+        'USER': 'RAVIN',
+        'PASSWORD': os.environ.get('DB_PASSWORD', ''),
+        'HOST': '127.0.0.1',
+        'PORT': '5432'
     }
 }
 
